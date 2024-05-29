d501491ecd5a65d16e93fd9c4bb4ac98fcdb0709
v.boursinos <v.boursinos@turintech.ai>
2024-05-29 17:43:13 +0100

    chore: add new test

--- src/test/java/KdbTest.java
+++ src/test/java/KdbTest.java
@@ -110,6 +110,34 @@ public class KdbTest {
         Assertions.assertTrue(columnSize == expectedColumnSize);
     }
 
+    @Test
+    public void testCode4() throws IOException, c.KException {
+        int expectedColumnSize = 11;
+        int expectedRowCount = 4444445;
+
+        Path path = Paths.get("src/main/resources/q/query_4.q");
+        String query = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
+        RunQ.executeScript(query, con);
+        c.Flip table = (c.Flip) con.k("10#joinTable");
+        for (int i = 0; i < c.n(table.y[0]); i++) {
+            for (int j = 0; j < table.y.length; j++) {
+                System.out.print(c.at(table.y[j], i) + " ");
+            }
+            System.out.println();
+        }
+
+        // Find the count of the joined table
+        long count = (long) con.k("count joinTable");
+        System.out.println("Count of joinTable: " + count);
+
+        Assertions.assertTrue(count == expectedRowCount);
+
+        int columnSize = table.y.length;
+        System.out.println("Column size: " + columnSize);
+        Assertions.assertTrue(columnSize == expectedColumnSize);
+    }
+
+
     @AfterAll
     public static void tearDown() throws IOException, c.KException {
         // close connection
