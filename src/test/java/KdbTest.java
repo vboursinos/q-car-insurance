e00ae2ab68a94650a73e7d50035466fb4463c50d
v.boursinos <v.boursinos@turintech.ai>
2024-05-30 11:23:26 +0100

    chore: add more checks at unit tests

--- src/test/java/KdbTest.java
+++ src/test/java/KdbTest.java
@@ -12,6 +12,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class KdbTest {
@@ -33,11 +35,13 @@ public class KdbTest {
     public void testCode1() throws IOException, c.KException {
         int expectedColumnSize = 11;
         int expectedRowCount = 4444445;
+        List<String> columnNames = Arrays.asList("id", "city", "country", "pop", "localTime", "continent", "region", "avgTemperature", "capital", "currency", "popUpdated");
 
         Path path = Paths.get("src/main/resources/q/query_1.q");
         String query = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
         RunQ.executeScript(query, con);
         c.Flip table = (c.Flip) con.k("10#joinTable");
+
         for (int i = 0; i < c.n(table.y[0]); i++) {
             for (int j = 0; j < table.y.length; j++) {
                 System.out.print(c.at(table.y[j], i) + " ");
@@ -53,12 +57,19 @@ public class KdbTest {
         int columnSize = table.y.length;
         System.out.println("Column size: " + columnSize);
         Assertions.assertTrue(columnSize == expectedColumnSize);
+
+        for (Object column : table.x) {
+            String columnName = (String) column;
+            Assertions.assertTrue(columnNames.contains(columnName));
+        }
+
     }
 
     @Test
     public void testCode2() throws IOException, c.KException {
         int expectedColumnSize = 9;
         int expectedRowCount = 8000000;
+        List<String> columnNames = Arrays.asList("id", "firstName", "lastName", "sales", "saleId", "age", "saleDate", "saleAmount", "dummy");
 
         Path path = Paths.get("src/main/resources/q/query_2.q");
         String query = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
@@ -79,12 +90,18 @@ public class KdbTest {
         int columnSize = table.y.length;
         System.out.println("Column size: " + columnSize);
         Assertions.assertTrue(columnSize == expectedColumnSize);
+
+        for (Object column : table.x) {
+            String columnName = (String) column;
+            Assertions.assertTrue(columnNames.contains(columnName));
+        }
     }
 
     @Test
     public void testCode3() throws IOException, c.KException {
         int expectedColumnSize = 4;
         int expectedRowCount = 3;
+        List<String> columnNames = Arrays.asList("dt", "sym", "book", "size");
 
         Path path = Paths.get("src/main/resources/q/query_3.q");
         String query = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
@@ -105,12 +122,18 @@ public class KdbTest {
         int columnSize = table.y.length;
         System.out.println("Column size: " + columnSize);
         Assertions.assertTrue(columnSize == expectedColumnSize);
+
+        for (Object column : table.x) {
+            String columnName = (String) column;
+            Assertions.assertTrue(columnNames.contains(columnName));
+        }
     }
 
     @Test
     public void testCode4() throws IOException, c.KException {
         int expectedColumnSize = 11;
         int expectedRowCount = 4444445;
+        List<String> columnNames = Arrays.asList("id", "city", "country", "pop", "localTime", "continent", "region", "avgTemperature", "capital", "currency", "popUpdated");
 
         Path path = Paths.get("src/main/resources/q/query_4.q");
         String query = new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
@@ -131,12 +154,18 @@ public class KdbTest {
         int columnSize = table.y.length;
         System.out.println("Column size: " + columnSize);
         Assertions.assertTrue(columnSize == expectedColumnSize);
+
+        for (Object column : table.x) {
+            String columnName = (String) column;
+            Assertions.assertTrue(columnNames.contains(columnName));
+        }
     }
 
     @Test
     public void testCode1FromMethod() throws c.KException, IOException {
         int expectedColumnSize = 11;
         int expectedRowCount = 4444445;
+        List<String> columnNames = Arrays.asList("id", "city", "country", "pop", "localTime", "continent", "region", "avgTemperature", "capital", "currency", "popUpdated");
 
         try {
             RunQFromMethod.joinCityCountry(con);
@@ -156,12 +185,18 @@ public class KdbTest {
         int columnSize = table.y.length;
         System.out.println("Column size: " + columnSize);
         Assertions.assertTrue(columnSize == expectedColumnSize);
+
+        for (Object column : table.x) {
+            String columnName = (String) column;
+            Assertions.assertTrue(columnNames.contains(columnName));
+        }
     }
 
     @Test
     public void testCode2FromMethod() throws c.KException, IOException {
         int expectedColumnSize = 9;
         int expectedRowCount = 8000000;
+        List<String> columnNames = Arrays.asList("id", "firstName", "lastName", "sales", "saleId", "age", "saleDate", "saleAmount", "dummy");
 
         try {
             RunQFromMethod.joinPersonSale(con);
@@ -169,7 +204,7 @@ public class KdbTest {
             e.printStackTrace();
         }
 
-        c.Flip table = null;
+        c.Flip table;
 
         table = (c.Flip) con.k("10#joinTable2");
 
@@ -181,12 +216,18 @@ public class KdbTest {
         int columnSize = table.y.length;
         System.out.println("Column size: " + columnSize);
         Assertions.assertTrue(columnSize == expectedColumnSize);
+
+        for (Object column : table.x) {
+            String columnName = (String) column;
+            Assertions.assertTrue(columnNames.contains(columnName));
+        }
     }
 
     @Test
     public void testCode3FromMethod() throws c.KException, IOException {
         int expectedColumnSize = 7;
         int expectedRowCount = 1;
+        List<String> columnNames = Arrays.asList("dt", "sym", "dummy", "size", "price", "sector", "employees");
 
         try {
             RunQFromMethod.joinTradeStock(con);
@@ -206,12 +247,19 @@ public class KdbTest {
         int columnSize = table.y.length;
         System.out.println("Column size: " + columnSize);
         Assertions.assertTrue(columnSize == expectedColumnSize);
+
+        for (Object column : table.x) {
+            String columnName = (String) column;
+            Assertions.assertTrue(columnNames.contains(columnName));
+        }
     }
 
     @Test
     public void testCode4FromMethod() throws c.KException, IOException {
         int expectedColumnSize = 10;
         int expectedRowCount = 4444445;
+        List<String> columnNames = Arrays.asList("id", "city", "country", "pop", "localTime", "continent", "region", "avgTemperature", "capital", "currency");
+
 
         try {
             RunQFromMethod.joinCityCountry2(con);
@@ -237,6 +285,12 @@ public class KdbTest {
         int columnSize = table.y.length;
         System.out.println("Column size: " + columnSize);
         Assertions.assertTrue(columnSize == expectedColumnSize);
+
+        for (Object column : table.x) {
+            System.out.println(column);
+            String columnName = (String) column;
+            Assertions.assertTrue(columnNames.contains(columnName));
+        }
     }
 
     @AfterAll
